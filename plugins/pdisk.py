d0b10b3cd1cecac40c441991385ff0f7e3aaceb1
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-10-09 18:32:59 +0530

    Update pdisk.py

--- plugins/pdisk.py
+++ plugins/pdisk.py
@@ -47,7 +47,7 @@ async def pdisk(bot, message):
             await message.reply_chat_action("typing")
             pdisk = "https://cofilink.com/share-video?videoid="+id      
             await message.reply_photo(
-                photo="https://telegra.ph/file/7389a320199583a2ac493.jpg",
+                photo="https://telegra.ph/file/997c2f126441b1b9c55c0.jpg",
                 caption="\n**URL:** `"+pdisk+"`\n\n**Your Pdisk link Generated\n━━━━━━━━━━━━━━━━━━━━\n⦿ Made With 💕By @Tellybots_4u\n━━━━━━━━━━━━━━━━━━━━**",
                 reply_markup=InlineKeyboardMarkup([
                     [ InlineKeyboardButton(text="🔗 PDisk Link", url=f"{pdisk}"), InlineKeyboardButton(text="Channel", url="https://telegram.me/tellybots_4u")]
